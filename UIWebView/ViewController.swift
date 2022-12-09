3dac4f8d2999be071137d7f4bd6f5e05ba1084ad
amitsahu88 <33515013+amitsahu88@users.noreply.github.com>
2022-12-09 19:44:24 +0530

    Code supported to Swift 4.2 UIWebView Replaced with WKWebView

--- UIWebView/ViewController.swift
+++ UIWebView/ViewController.swift
@@ -9,113 +9,74 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, UIWebViewDelegate, WKNavigationDelegate {
+class ViewController: UIViewController{
     
-  
-    @IBOutlet weak var webView: UIWebView!
- 
-    @IBOutlet weak var myProgressView: UIProgressView!
-    
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var barBackButton: UIBarButtonItem!
-    
     @IBOutlet weak var barForwardButton: UIBarButtonItem!
     @IBOutlet weak var barReloadButton: UIBarButtonItem!
     @IBOutlet weak var toolbar: UIToolbar!
+    let loadingKey = "loading"
+    let estimatedProgressKey = "estimatedProgress"
     
-    var myTimer: NSTimer = NSTimer()
-    var theBool: Bool = Bool()
-
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        webView.delegate = self
-        
-      
-        
-        myProgressView.trackTintColor = UIColor.whiteColor()
-        
-        barBackButton.enabled = false
-        barForwardButton.enabled = false
-        
-        webView.addObserver(self, forKeyPath: "loading", options: .New, context: nil)
-        
-        let url = NSURL(string: "https://developer.apple.com/")
-        let request = NSURLRequest(URL:url!)
-        webView.loadRequest(request)
-        
         self.title = "webview"
-        
-        toolbar.shadowImageForToolbarPosition(UIBarPosition.Top)
+        // Do any additional setup after loading the view, typically from a nib.
+        self.webView.navigationDelegate = self
+        self.webView.addObserver(self, forKeyPath: self.loadingKey, options: .new, context: nil)
+        self.webView.addObserver(self, forKeyPath: self.estimatedProgressKey, options: .new, context: nil)
+
+        self.loadURL()
         
     }
 
-    override func viewDidAppear(animated: Bool) {
-        webViewDidStartLoad(webView)
-    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func timerCallback(){
-        
-        if webView.canGoBack {
-            barBackButton.enabled = true
+    fileprivate func loadURL(){
+        if let url = URL(string: "https://developer.apple.com/"){
+            let request = URLRequest(url: url)
+            self.webView.load(request)
         }
-        
-        if webView.canGoForward {
-            barForwardButton.enabled = true
-        }
-       
-        if theBool {
-            if myProgressView.progress >= 1 {
-                myProgressView.hidden = true
-                myTimer.invalidate()
-              
-            }else{
-                myProgressView.progress += 0.1
-            }
-        }else{
-            myProgressView.progress += 0.05
-            if myProgressView.progress >= 0.95 {
-                myProgressView.progress = 0.95
-            }
-        }
-        
     }
     
-    func webViewDidStartLoad(webView: UIWebView) {
-        self.myProgressView.hidden = false
-        myProgressView.progress = 0.0
-        theBool = false
-        myTimer =  NSTimer.scheduledTimerWithTimeInterval(0.01667,target: self,selector: #selector(ViewController.timerCallback),userInfo: nil,repeats: true)
-        
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        if (keyPath == self.loadingKey) {
+            self.barBackButton?.isEnabled = webView.canGoBack
+            self.barForwardButton?.isEnabled = webView.canGoForward
+        }
+        if (keyPath == self.estimatedProgressKey) {
+            self.progressBar?.isHidden = webView?.estimatedProgress == 1
+            self.progressBar?.setProgress(Float(webView.estimatedProgress), animated: true)
+        }
     }
-    
-    func webViewDidFinishLoad(webView: UIWebView) {
-      
-        self.theBool = true
-    }
-    
-  
+
     @IBAction func barBackButtonAction(sender: AnyObject) {
-        if webView.canGoBack {
-             webView.goBack()
+        if self.webView.canGoBack {
+            self.webView.goBack()
         }
-       
     }
     
-  
     @IBAction func barForwardButtonAction(sender: AnyObject) {
-        if webView.canGoForward{
-            webView.goForward()
+        if self.webView.canGoForward{
+            self.webView.goForward()
         }
     }
 
     @IBAction func barReloadButtonAction(sender: AnyObject) {
-        webView.reload()
+        if let webUrl = webView.url{
+            let request = URLRequest(url: webUrl)
+            self.webView.load(request)
+        }
     }
 }
 
+extension ViewController:WKNavigationDelegate{
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.progressBar.setProgress(0.0, animated: false)
+    }
+}
